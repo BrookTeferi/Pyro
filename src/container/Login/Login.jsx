300130f4991a5202a291cb2e5d871ae845a7d1a1
Brook Teferi <brookteferi1q2w@gmail.com>
2022-10-09 11:22:50 +0300

    Userlogin

---
+++ src/container/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { images } from '../../constants';
+import './Login.css';
+
+const Login = () => (
+    <div className="app__login app__bg flex__center section__padding" id="about">
+    <div className="app__login-overlay flex__center">
+      <img src={images.G} alt="G_overlay" />
+    </div>
+    </div>
+    );
+
+export default AboutUs;
